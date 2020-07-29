4fd30e2cb03ab1ccb88a1e11f83aac3954f89392
suzukiplan <suzukiplan.ys@gmail.com>
2020-07-29 21:41:54 +0900

    set flag before execting test

--- test-clock-gb.cpp
+++ test-clock-gb.cpp
@@ -14,7 +14,7 @@ void writeByte(void* arg, unsigned short addr, unsigned char value) { ((MMU*)arg
 static int testNumber;
 static int expectClocks;
 static FILE* file;
-void executeTest(Z80* cpu, MMU* mmu, unsigned char op1, unsigned char op2, unsigned char op3, unsigned char op4, int clocks)
+void executeTest(Z80* cpu, MMU* mmu, unsigned char op1, unsigned char op2, unsigned char op3, unsigned char op4, unsigned char flagBeofre, int clocks)
 {
     expectClocks = clocks;
     mmu->RAM[0] = op1;
@@ -22,6 +22,7 @@ void executeTest(Z80* cpu, MMU* mmu, unsigned char op1, unsigned char op2, unsig
     mmu->RAM[2] = op3;
     mmu->RAM[3] = op4;
     cpu->reg.PC = 0;
+    cpu->reg.pair.F = flagBeofre;
     int actualClocks = cpu->execute(1);
     if (expectClocks != actualClocks) {
         fprintf(file, "TEST FAILED! (expected=%dHz, actual=%dHz)\n", expectClocks, actualClocks);
@@ -46,38 +47,38 @@ int main(int argc, char* argv[])
     });
 
     // checked clocks from https://pastraiser.com/cpu/gameboy/gameboy_opcodes.html
-    executeTest(&z80, &mmu, 0x00, 0, 0, 0, 4);        // NOP
-    executeTest(&z80, &mmu, 0x01, 0x34, 0x12, 0, 12); // LD BC,d16
-    executeTest(&z80, &mmu, 0x02, 0, 0, 0, 8);        // LD (BC),A
-    executeTest(&z80, &mmu, 0x03, 0, 0, 0, 8);        // INC BC
-    executeTest(&z80, &mmu, 0x04, 0, 0, 0, 4);        // INC B
-    executeTest(&z80, &mmu, 0x05, 0, 0, 0, 4);        // DEC B
-    executeTest(&z80, &mmu, 0x06, 123, 0, 0, 8);      // LD B,d8
-    executeTest(&z80, &mmu, 0x07, 0, 0, 0, 4);        // RLCA
-    executeTest(&z80, &mmu, 0x08, 0xCD, 0xAB, 0, 20); // LD (a16),SP
-    executeTest(&z80, &mmu, 0x09, 0, 0, 0, 8);        // ADD HL,BC
-    executeTest(&z80, &mmu, 0x0A, 0, 0, 0, 8);        // LD A,(BC)
-    executeTest(&z80, &mmu, 0x0B, 0, 0, 0, 8);        // DEC BC
-    executeTest(&z80, &mmu, 0x0C, 0, 0, 0, 4);        // INC C
-    executeTest(&z80, &mmu, 0x0D, 0, 0, 0, 4);        // DEC C
-    executeTest(&z80, &mmu, 0x0E, 98, 0, 0, 8);       // LD C,d8
-    executeTest(&z80, &mmu, 0x0F, 0, 0, 0, 4);        // RRCA
-    executeTest(&z80, &mmu, 0x10, 0, 0, 0, 4);        // STOP
-    executeTest(&z80, &mmu, 0x11, 0x34, 0x12, 0, 12); // LD DE,d16
-    executeTest(&z80, &mmu, 0x12, 0xCD, 0, 0, 8);     // LD (DE),A
-    executeTest(&z80, &mmu, 0x13, 0, 0, 0, 8);        // INC DE
-    executeTest(&z80, &mmu, 0x14, 0, 0, 0, 4);        // INC D
-    executeTest(&z80, &mmu, 0x15, 0, 0, 0, 4);        // DEC D
-    executeTest(&z80, &mmu, 0x16, 3, 0, 0, 8);        // LD D,d8
-    executeTest(&z80, &mmu, 0x17, 0, 0, 0, 4);        // RLA
-    executeTest(&z80, &mmu, 0x18, 0x80, 0, 0, 12);    // JR r8
-    executeTest(&z80, &mmu, 0x19, 3, 0, 0, 8);        // ADD HL,DE
-    executeTest(&z80, &mmu, 0x1A, 3, 0, 0, 8);        // LD A,(DE)
-    executeTest(&z80, &mmu, 0x1B, 3, 0, 0, 8);        // DEC DE
-    executeTest(&z80, &mmu, 0x1C, 0, 0, 0, 4);        // INC D
-    executeTest(&z80, &mmu, 0x1D, 0, 0, 0, 4);        // DEC D
-    executeTest(&z80, &mmu, 0x1E, 12, 0, 0, 8);       // LD E,d8
-    executeTest(&z80, &mmu, 0x1F, 0, 0, 0, 4);        // RRA
+    executeTest(&z80, &mmu, 0x00, 0, 0, 0, 0, 4);        // NOP
+    executeTest(&z80, &mmu, 0x01, 0x34, 0x12, 0, 0, 12); // LD BC,d16
+    executeTest(&z80, &mmu, 0x02, 0, 0, 0, 0, 8);        // LD (BC),A
+    executeTest(&z80, &mmu, 0x03, 0, 0, 0, 0, 8);        // INC BC
+    executeTest(&z80, &mmu, 0x04, 0, 0, 0, 0, 4);        // INC B
+    executeTest(&z80, &mmu, 0x05, 0, 0, 0, 0, 4);        // DEC B
+    executeTest(&z80, &mmu, 0x06, 123, 0, 0, 0, 8);      // LD B,d8
+    executeTest(&z80, &mmu, 0x07, 0, 0, 0, 0, 4);        // RLCA
+    executeTest(&z80, &mmu, 0x08, 0xCD, 0xAB, 0, 0, 20); // LD (a16),SP
+    executeTest(&z80, &mmu, 0x09, 0, 0, 0, 0, 8);        // ADD HL,BC
+    executeTest(&z80, &mmu, 0x0A, 0, 0, 0, 0, 8);        // LD A,(BC)
+    executeTest(&z80, &mmu, 0x0B, 0, 0, 0, 0, 8);        // DEC BC
+    executeTest(&z80, &mmu, 0x0C, 0, 0, 0, 0, 4);        // INC C
+    executeTest(&z80, &mmu, 0x0D, 0, 0, 0, 0, 4);        // DEC C
+    executeTest(&z80, &mmu, 0x0E, 98, 0, 0, 0, 8);       // LD C,d8
+    executeTest(&z80, &mmu, 0x0F, 0, 0, 0, 0, 4);        // RRCA
+    executeTest(&z80, &mmu, 0x10, 0, 0, 0, 0, 4);        // STOP
+    executeTest(&z80, &mmu, 0x11, 0x34, 0x12, 0, 0, 12); // LD DE,d16
+    executeTest(&z80, &mmu, 0x12, 0xCD, 0, 0, 0, 8);     // LD (DE),A
+    executeTest(&z80, &mmu, 0x13, 0, 0, 0, 0, 8);        // INC DE
+    executeTest(&z80, &mmu, 0x14, 0, 0, 0, 0, 4);        // INC D
+    executeTest(&z80, &mmu, 0x15, 0, 0, 0, 0, 4);        // DEC D
+    executeTest(&z80, &mmu, 0x16, 3, 0, 0, 0, 8);        // LD D,d8
+    executeTest(&z80, &mmu, 0x17, 0, 0, 0, 0, 4);        // RLA
+    executeTest(&z80, &mmu, 0x18, 0x80, 0, 0, 0, 12);    // JR r8
+    executeTest(&z80, &mmu, 0x19, 3, 0, 0, 0, 8);        // ADD HL,DE
+    executeTest(&z80, &mmu, 0x1A, 3, 0, 0, 0, 8);        // LD A,(DE)
+    executeTest(&z80, &mmu, 0x1B, 3, 0, 0, 0, 8);        // DEC DE
+    executeTest(&z80, &mmu, 0x1C, 0, 0, 0, 0, 4);        // INC D
+    executeTest(&z80, &mmu, 0x1D, 0, 0, 0, 0, 4);        // DEC D
+    executeTest(&z80, &mmu, 0x1E, 12, 0, 0, 0, 8);       // LD E,d8
+    executeTest(&z80, &mmu, 0x1F, 0, 0, 0, 0, 4);        // RRA
     // TODO: execute other tests...
 
     return 0;
