9c5681fbfa272728df7906875bddddf1e31ad6c8
suzukiplan <suzukiplan.ys@gmail.com>
2022-09-01 17:40:49 +0900

    refactor: optimize `CP R` operands

--- z80.hpp
+++ z80.hpp
@@ -4098,6 +4098,13 @@ class Z80
     inline int CPDR() { return repeatCP(false, true); }
 
     // Compare Register
+    static inline int CP_B(Z80* ctx) { return ctx->CP_R(0b000); }
+    static inline int CP_C(Z80* ctx) { return ctx->CP_R(0b001); }
+    static inline int CP_D(Z80* ctx) { return ctx->CP_R(0b010); }
+    static inline int CP_E(Z80* ctx) { return ctx->CP_R(0b011); }
+    static inline int CP_H(Z80* ctx) { return ctx->CP_R(0b100); }
+    static inline int CP_L(Z80* ctx) { return ctx->CP_R(0b101); }
+    static inline int CP_A(Z80* ctx) { return ctx->CP_R(0b111); }
     inline int CP_R(unsigned char r)
     {
         if (isDebug()) log("[%04X] CP %s, %s", reg.PC, registerDump(0b111), registerDump(r));
@@ -4920,7 +4927,14 @@ class Z80
         opSet1[0b10110000 + 0b101] = OR_L;
         opSet1[0b10110000 + 0b110] = OR_HL;
         opSet1[0b10110000 + 0b111] = OR_A;
-        opSet1[0b10111110] = CP_HL;
+        opSet1[0b10111000 + 0b000] = CP_B;
+        opSet1[0b10111000 + 0b001] = CP_C;
+        opSet1[0b10111000 + 0b010] = CP_D;
+        opSet1[0b10111000 + 0b011] = CP_E;
+        opSet1[0b10111000 + 0b100] = CP_H;
+        opSet1[0b10111000 + 0b101] = CP_L;
+        opSet1[0b10111000 + 0b110] = CP_HL;
+        opSet1[0b10111000 + 0b111] = CP_A;
         opSet1[0b11000011] = JP_NN;
         opSet1[0b11000110] = ADD_A_N;
         opSet1[0b11001001] = RET;
@@ -5243,8 +5257,6 @@ class Z80
                         ret = RST((operandNumber & 0b00111000) >> 3, true);
                     } else if ((operandNumber & 0b11000000) == 0b01000000) {
                         ret = LD_R1_R2((operandNumber & 0b00111000) >> 3, operandNumber & 0b00000111);
-                    } else if ((operandNumber & 0b11111000) == 0b10111000) {
-                        ret = CP_R(operandNumber & 0b00000111);
                     } else if (skipIllegalInstructions) {
                         if (isDebug()) log("Skipped an illegal instruction: $%02X", op);
                         reg.PC += 1;
