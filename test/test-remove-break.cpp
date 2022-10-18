292a23b58d083898553242d505a7011dffcadbdc
suzukiplan <suzukiplan.ys@gmail.com>
2022-10-18 15:58:39 +0900

    add test: set/reset - DebugMessage/ConsumeClockCallback

--- test/test-remove-break.cpp
+++ test/test-remove-break.cpp
@@ -9,7 +9,6 @@ int main()
         0xED, 0x78,       // IN A, (C)
         0xc3, 0x00, 0x00, // JMP $0000
     };
-
     Z80 z80([&rom](void* arg, unsigned short addr) {
         return rom[addr & 0xFF];
     }, [](void* arg, unsigned char addr, unsigned char value) {
@@ -27,6 +26,10 @@ int main()
             });
         }
     }
+    long totalClocks = 0;
+    z80.setConsumeClockCallback([&totalClocks](void* arg, int clocks) {
+        totalClocks = clocks + totalClocks;
+    });
     for (int operand = 0; operand < 65536; operand++) {
         unsigned char op1 = (unsigned char)((operand & 0xFF00) >> 8);
         unsigned char op2 = (unsigned char)(operand & 0xFF);
@@ -51,15 +54,27 @@ int main()
     puts("\n===== remove break point 3 and 7 =====\n");
     z80.removeBreakPoint(3);
     z80.removeBreakPoint(7);
-    z80.execute(50);
+    printf("executed: %dHz\n", z80.execute(50));
+    printf("total clocks = %ldHz\n", totalClocks);
     puts("\n===== remove all break points =====\n");
     z80.removeAllBreakPoints();
-    z80.execute(50);
+    printf("executed: %dHz\n", z80.execute(50));
+    printf("total clocks = %ldHz\n", totalClocks);
     puts("\n===== remove break operand ED =====\n");
     z80.removeBreakOperand(0xED);
-    z80.execute(50);
+    printf("executed: %dHz\n", z80.execute(50));
+    printf("total clocks = %ldHz\n", totalClocks);
     puts("\n===== remove all break operands =====\n");
     z80.removeAllBreakOperands();
-    z80.execute(50);
+    printf("executed: %dHz\n", z80.execute(50));
+    printf("total clocks = %ldHz\n", totalClocks);
+    puts("\n===== reset debug message =====\n");
+    z80.resetDebugMessage();
+    printf("executed: %dHz\n", z80.execute(50));
+    printf("total clocks = %ldHz\n", totalClocks);
+    puts("\n===== reset consume clock callback =====\n");
+    z80.resetConsumeClockCallback();
+    printf("executed: %dHz\n", z80.execute(50));
+    printf("total clocks = %ldHz\n", totalClocks);
     return 0;
 }
